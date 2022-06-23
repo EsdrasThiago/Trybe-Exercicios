bc850fae4517b6f619b958ad1d706c41d6eef819
Esdras <esdras12thiago@gmail.com>
2022-06-23 18:36:49 -0300

    Primeiro exercicio

---
+++ fundamentos/bloco4/dia-3-introducao-a-javascript-e-logica-de-programacao/exercicio.js
@@ -0,0 +1,14 @@
+//1 
+
+let fatorial = 10;
+if (fatorial < 0) {
+    
+} else if (fatorial === 0) {
+    
+} else {
+    let fator = 1;
+    for (i = 1; i <= fatorial; i++) {
+        fator *= i;
+    }
+    console.log(fator)
+}
