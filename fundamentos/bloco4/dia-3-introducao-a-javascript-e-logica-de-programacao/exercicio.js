27f041567f109120ec609986c9b1c009a95bee82
Esdras <esdras12thiago@gmail.com>
2022-06-23 18:44:07 -0300

    Segundo exercicio

--- fundamentos/bloco4/dia-3-introducao-a-javascript-e-logica-de-programacao/exercicio.js
+++ fundamentos/bloco4/dia-3-introducao-a-javascript-e-logica-de-programacao/exercicio.js
@@ -1,14 +1,19 @@
 //1 
 
-let fatorial = 10;
-if (fatorial < 0) {
+// let fatorial = 10;
+// if (fatorial < 0) {
     
-} else if (fatorial === 0) {
+// } else if (fatorial === 0) {
     
-} else {
-    let fator = 1;
-    for (i = 1; i <= fatorial; i++) {
-        fator *= i;
-    }
-    console.log(fator)
-}
+// } else {
+//     let fator = 1;
+//     for (i = 1; i <= fatorial; i++) {
+//         fator *= i;
+//     }
+//     console.log(fator)
+// }
+
+//2
+
+let word = 'tryber';
+console.log([...word].reverse().join(''));
