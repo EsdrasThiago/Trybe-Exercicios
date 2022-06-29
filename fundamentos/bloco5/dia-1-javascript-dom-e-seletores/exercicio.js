441fa8ddc42da3e8f530903afd76f7401bf275c4
Esdras <esdras12thiago@gmail.com>
2022-06-29 12:07:01 -0300

    Segundo requisito

--- fundamentos/bloco5/dia-1-javascript-dom-e-seletores/exercicio.js
+++ fundamentos/bloco5/dia-1-javascript-dom-e-seletores/exercicio.js
@@ -1,1 +1,12 @@
+//1
+
 document.getElementById('futuro').innerText = "Planejo estar vivendo uma vida tranquila em sp";
+
+//2
+
+function background(cor) {
+    let corFundo = document.getElementsByClassName('main-content')[0];
+    corFundo.style.background = cor
+}
+
+background('rgb(76,164,109)')
